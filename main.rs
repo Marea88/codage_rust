a73555d26d94faa0ab9712891027ca34fef2c705
Marea88 <mathieu.ar@hotmail.fr>
2018-07-12 16:12:22 +0200

    Added doc

--- main.rs
+++ main.rs
@@ -19,7 +19,7 @@ impl Iterator for Fibonacci {
         self.curr = self.next;
         self.next = new_next;
 
-        // Since there's no endpoint to a Fibonacci sequence, the `Iterator`
+        // Since there's no endpoint to a Fibonnaci sequence, the `Iterator`
         // will never return `None`, and `Some` is always returned.
         Some(self.curr)
     }
@@ -41,11 +41,11 @@ fn fib_code(num: u64) -> String {
         .take_while(|&x| num > x)
         .collect::<Vec<_>>()
         .into_iter()
-        .rev();
+        .rev(); //We use the iterator FIB to produce the value we need then reverse it to start with the biggest value.
 
     let mut rest = num - suite.next().unwrap();
     let mut code = "1".to_string();
-
+    
     for i in suite {
         if rest > i && code.chars().last().unwrap() == '0' {
             rest -= i;
@@ -54,18 +54,16 @@ fn fib_code(num: u64) -> String {
             code.push('0');
         }
     }
-    code.chars().rev().chain("1".chars()).collect()
+    code.chars().rev().chain("1".chars()).collect() // We reverse the chain and return it.
 }
 
 
 fn fib_decode(x: &str) -> u64{
-        x[..x.len()-1].chars()
-        .zip(FIB)
-        .filter(|x| x.0 == '1')
-        .fold(0, |acc, (_, x)| acc + x)
+    x[..x.len()-1].chars()
+    .zip(FIB)
+    .filter(|x| x.0 == '1')
+    .fold(0, |acc, (_, x)| acc + x)
 }
-
-
 
 fn main() {
     let _f = frequency("abaabcd");
